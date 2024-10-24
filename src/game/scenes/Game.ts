8af76eba111f91d2173d953d5d03adc83b560416
filwas <fwas.corp@gmail.com>
2024-10-24 16:47:30 +0200

    import sprites

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,36 +1,77 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
+export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     gameText: Phaser.GameObjects.Text;
+    platforms!: Phaser.Physics.Arcade.StaticGroup;
+    player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
-    constructor ()
-    {
-        super('Game');
+    constructor() {
+        super("Game");
     }
 
-    create ()
-    {
+    preload() {
+        this.load.image("sky", "assets/sky.png");
+        this.load.image("ground", "assets/platform.png");
+        this.load.image("star", "assets/star.png");
+        this.load.image("bomb", "assets/bomb.png");
+    }
+
+    create() {
         this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        this.add.image(400, 300, "sky");
+        this.platforms = this.physics.add.staticGroup();
 
-        this.gameText = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
 
-        EventBus.emit('current-scene-ready', this);
+        this.platforms.create(600, 400, "ground");
+        this.platforms.create(50, 250, "ground");
+        this.platforms.create(750, 220, "ground");
+
+        this.player = this.physics.add.sprite(100, 450, "dude");
+        this.player.setInteractive();
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        if (this.anims.exists("turn")) {
+            this.player.anims.play("turn");
+        } else {
+            console.error("Animation 'turn' not found!");
+        }
+
+        EventBus.emit("current-scene-ready", this);
     }
 
-    changeScene ()
-    {
-        this.scene.start('GameOver');
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play("left", true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play("right", true);
+        } else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play("turn");
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
+    }
+
+    changeScene() {
+        this.scene.start("GameOver");
     }
 }
+
